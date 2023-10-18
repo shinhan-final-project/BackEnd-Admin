a190cd1982e64dc5510ec6c96c85da44739528dd
Lee Sohyeong <69043819+sohyeong-dev@users.noreply.github.com>
2023-10-19 01:27:28 +0900

    주가 예측 게임
    * feat: 종목 가져오기
    
    * feat: 주가 예측 게임 만들기
    
    * feat: (주가 예측 게임) 수익률 저장
    
    * feat: (주가 예측 게임) 주가 불러오기 API 예외 처리
    
    * feat: 주가 예측 게임
    
    ---------
    
    Co-authored-by: isohyeong <so-hyeong@naver.com>

--- src/main/java/com/shinhan/friends_stock_admin/repository/InvestItemRepository.java
+++ src/main/java/com/shinhan/friends_stock_admin/repository/InvestItemRepository.java
@@ -3,7 +3,10 @@ package com.shinhan.friends_stock_admin.repository;
 import com.shinhan.friends_stock_admin.domain.entity.InvestItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InvestItemRepository extends JpaRepository<InvestItem, Long> {
     boolean existsByStockCode(int stockCode);
 
+    List<InvestItem> findByIsPublished(boolean b);
 }
